a158d9c39213b76d3ea00fa4a255cd0cb1611399
Hacı Celal Aygar <celal.aygar@gmail.com>
2023-05-18 03:05:36 +0300

    Create GetDomainNameInformationV1.py

---
+++ 8-Get-Domain-Name-Information/GetDomainNameInformationV1.py
@@ -0,0 +1,29 @@
+import whois # pip install python-whois
+
+def is_registered(domain_name):
+    """
+    A function that returns a boolean indicating 
+    whether a `domain_name` is registered
+    """
+    try:
+        w = whois.whois(domain_name)
+    except Exception:
+        return False
+    else:
+        return bool(w.domain_name)
+      
+      
+domains = [
+    "thepythoncode.com",
+    "google.com",
+    "github.com",
+    "unknownrandomdomain.com",
+    "notregistered.co"
+]
+# iterate over domains
+for domain in domains:
+    print(domain, "is registered" if is_registered(domain) else "is not registered")
+      
+      
+      
+      
