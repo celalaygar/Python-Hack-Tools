e10fd79d04ef8f1881df0336bfcb258d5d5fff73
Hacı Celal Aygar <celal.aygar@gmail.com>
2023-07-09 03:15:15 +0300

    Update BruteForceFTP_V2.py

--- 9-Brute-Force-FTP/BruteForceFTP_V2.py
+++ 9-Brute-Force-FTP/BruteForceFTP_V2.py
@@ -1,7 +1,6 @@
 # Python dilinde bir FTP sunucusuna brute force saldırısı yapmak için basit bir örnek 
 # kod bulunmaktadır. Ancak, lütfen unutmayın ki, bu tür saldırılar yasa dışıdır ve 
-# başka kişilerin veya sistemlerin 
-# izni olmadan gerçekleştirilmemelidir. 
+# başka kişilerin veya sistemlerin izni olmadan gerçekleştirilmemelidir. 
 # Bu kodu yasal ve etik olarak kabul edilebilir bir şekilde kullanmalısınız.
 
 
